696ca5e02671abf82b6ec89f79fe4ddfff3b514e
shamalm <shamalm@siddhatech.com>
2023-12-18 19:08:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# katalon_CI_CD_v1
+Starting of CI/CD
---
+++ Test Suites/Form_filling.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Form_filling</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>61f42ad1-6e5f-47d0-bb50-fbbf03f8b527</testSuiteGuid>
+   <testCaseLink>
+      <guid>462e5377-cede-4c95-8a7a-6de4defabeb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Form</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
